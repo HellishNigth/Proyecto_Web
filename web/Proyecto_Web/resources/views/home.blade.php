16ed08876736cf05dcd9069f772dd2ce063be7cd
Ignacio <kritka200@gmail.com>
2021-07-15 13:34:44 -0400

    Avance 2: hasta el token

--- web/Proyecto_Web/resources/views/home.blade.php
+++ web/Proyecto_Web/resources/views/home.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.master')
 
 @section('contenido')
-    <h1>home</h1>
+    <div class="row mt-5">
+        <div class="col-12 col-md-6 col-lg-5 mx-auto">
+            <div class="card">
+                <div class="card-header bg-danger text-dark">
+                    <span>Agregar Producto</span>
+                </div>
+                <div class="card-body">
+                    <div class="mb-3">
+                        <label for="nombre-txt" class="form-label">Nombre Producto</label>
+                        <input type="text" id="nombreproduc-txt" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="tipo_produc-select" class="form-label">Tipo Producto</label>
+                        <select class="form-select" id="tipo_produc-select">
+                            <option value="comida">Comida</option>
+                            <option value="limpieza">Limpieza</option>
+                            <option value="medicamento">Medicamento</option>
+                            <option value="electronico">Electronico</option>
+                
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="cantidad-txt" class="form-label">Cantidad</label>
+                        <input type="number" class="form-control" id="cantidad-txt">
+                    </div>
+                </div>
+                <div class="card-footer d-grid gap-1">
+                    <button class="btn btn-info">Registrar</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- web/Proyecto_Web/resources/views/layouts/master.blade.php
+++ web/Proyecto_Web/resources/views/layouts/master.blade.php
@@ -4,17 +4,19 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="csrf-token" content="{{csrf_token()}}">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{asset("css/style.css")}}">
     <title>Proyecto_Web</title>
   </head>
   <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-dark">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#">Proyecto_Web</a>
+              <a class="navbar-brand" href="{{route('home')}}">
+                <img class="logo" src="{{asset("img/logo.png")}}" />
+              </a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
@@ -39,7 +41,8 @@
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="{{asset('js/axios_config.js')}}"></script>
 
   </body>
 </html>
